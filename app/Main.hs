ba8ad8a6b55883ab8576524dbcbf9353e8ca67d4
Pleket <50240262+Pleket@users.noreply.github.com>
2023-07-06 16:08:47 +0200

    Fix

--- app/Main.hs
+++ app/Main.hs
@@ -12,8 +12,13 @@ main = do
     -- 1 + 2x + 3x^2 + 4x^3 * 3x^7
     print (showPoly 
         (addPoly 
-            (subtractPoly pol [0, 0, 0, 4]) 
-            (multPoly [0, 0, 0, 4] [0,0,0,0,0,0,0,3])
+            (subtractPoly pol [0, 0, 0, 0, 4])
+            (multPoly [0, 0, 0, 0, 4] [0,0,0,0,0,0,0,3])
+        ))
+    -- OR
+    print (showPoly (addPoly 
+            [1, -2, -3]
+            (multPoly [0, 0, 0, 0, 4] [0,0,0,0,0,0,0,3])
         ))
     
     -- (1 + 2x + 3x^2 + 4x^3) * 3x^7
